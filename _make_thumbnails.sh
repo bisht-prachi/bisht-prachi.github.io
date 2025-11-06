0c07a591ae8412f25dc2b47896f83746147f9554
Prachi Bisht <103419553+bisht-prachi@users.noreply.github.com>
2025-11-06 06:42:04 +0000

    edit thumbnail command

--- _make_thumbnails.sh
+++ _make_thumbnails.sh
@@ -1,3 +1,5 @@
-for file in images/*.{jpg,png}; do
-    [ ! -f "tn/$file" ] && convert "$file"  -thumbnail 160x160 "tn/$file"
+mkdir -p tn/images
+for file in images/*.jpg images/*.png; do
+  [ -e "$file" ] || continue
+  [ ! -f "tn/$file" ] && convert "$file" -thumbnail 160x160 "tn/$file"
 done
